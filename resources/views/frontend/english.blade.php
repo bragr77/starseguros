0079bbca39104dc0e784d30c222b8a00919299cc
Raul Gonzalez <bragr77@gmail.com>
2020-10-22 09:53:31 -0400

    arrglando bugs de estilos css

--- resources/views/frontend/english.blade.php
+++ resources/views/frontend/english.blade.php
@@ -13,10 +13,10 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet" type="text/css">
-    <link href="css/icons.css" rel="stylesheet" type="text/css">
 
 
     <!-- Estilos pagina -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/icons.css') }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/easy-responsive-tabs.css') }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/flexslider.css') }}"/>
     <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/owl.carousel.css') }}"/>
